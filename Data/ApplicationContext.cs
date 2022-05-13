756952c4cb1bb2a6637f8bd7bda808afebb211e9
Gabriel Nascimento <gabrielnascimento_17@hotmail.com>
2022-05-13 00:03:12 -0300

    refactor: after change name of props in client model, i change a code first to database

--- Data/ApplicationContext.cs
+++ Data/ApplicationContext.cs
@@ -5,22 +5,22 @@ namespace MonitoriaWEBAPI.Data
 {
     public class ApplicationContext : DbContext
     {
-        public DbSet<Cliente> Clientes { get; set; }
+        public DbSet<Client> Clients { get; set; }
         protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
         {
-            optionsBuilder.UseSqlServer("Data Source=DEVELOPMENT;Initial Catalog=MonitoriaApiWEB;Integrated Security=True");
+            optionsBuilder.UseSqlServer("Data Source=DEVELOPMENT;Initial Catalog=MonitoriaApiWEB;User Id=sa;Password=Biels@@1");
         }
 
         protected override void OnModelCreating(ModelBuilder modelBuilder)
         {
-            modelBuilder.Entity<Cliente>(table => {
+            modelBuilder.Entity<Client>(table => {
 
-                table.ToTable("Clientes");
-                table.Property("Nome").HasMaxLength(40);
-                table.Property("CPF").HasColumnType("CHAR(11)");
-                table.Property("Nascimento").HasColumnType("DATE");
+                table.ToTable("clients");
+                table.Property("NameAndSurname").HasMaxLength(40).HasField("name_and_surname");
+                table.Property("RegisterOfPhysicalPerson").HasColumnType("CHAR(11)").HasField("register_of_physical_person");
+                table.Property("DateOfBorn").HasColumnType("DATE").HasField("date_of_born");
 
-                table.HasKey("Id");
+                table.HasKey("ClientId").HasName("client_id");
             });   
         }
     }
