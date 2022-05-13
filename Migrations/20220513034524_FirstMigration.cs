e6f6c0621a5dd40c9115289ed55137dc103bdc75
Gabriel Nascimento <gabrielnascimento_17@hotmail.com>
2022-05-13 00:47:14 -0300

    fix: generate scripts and migrations

---
+++ Migrations/20220513034524_FirstMigration.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace MonitoriaWEBAPI.Migrations
+{
+    public partial class FirstMigration : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateTable(
+                name: "clients",
+                columns: table => new
+                {
+                    client_id = table.Column<int>(type: "INT", nullable: false)
+                        .Annotation("SqlServer:Identity", "1, 1"),
+                    name_and_surname = table.Column<string>(type: "nvarchar(40)", maxLength: 40, nullable: true),
+                    register_of_physical_person = table.Column<string>(type: "CHAR(11)", nullable: true),
+                    date_of_born = table.Column<DateTime>(type: "DATE", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_clients", x => x.client_id);
+                });
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "clients");
+        }
+    }
+}
---
+++ Migrations/Scripts/FirstMigration.sql
@@ -0,0 +1,29 @@
+﻿IF OBJECT_ID(N'[__EFMigrationsHistory]') IS NULL
+BEGIN
+    CREATE TABLE [__EFMigrationsHistory] (
+        [MigrationId] nvarchar(150) NOT NULL,
+        [ProductVersion] nvarchar(32) NOT NULL,
+        CONSTRAINT [PK___EFMigrationsHistory] PRIMARY KEY ([MigrationId])
+    );
+END;
+GO
+
+BEGIN TRANSACTION;
+GO
+
+CREATE TABLE [clients] (
+    [client_id] INT NOT NULL IDENTITY,
+    [name_and_surname] nvarchar(40) NULL,
+    [register_of_physical_person] CHAR(11) NULL,
+    [date_of_born] DATE NOT NULL,
+    CONSTRAINT [PK_clients] PRIMARY KEY ([client_id])
+);
+GO
+
+INSERT INTO [__EFMigrationsHistory] ([MigrationId], [ProductVersion])
+VALUES (N'20220513034524_FirstMigration', N'5.0.10');
+GO
+
+COMMIT;
+GO
+
