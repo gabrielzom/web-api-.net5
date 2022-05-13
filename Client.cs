6b689a3986e69bfab0fb0a9acbe57d54be843aca
Gabriel Nascimento <gabrielnascimento_17@hotmail.com>
2022-05-13 00:01:15 -0300

    refactor: change name of fields and table after migration

---
+++ Client.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace MonitoriaWEBAPI
+{
+    public class Client
+    {
+        public int Id { get; set; }
+        public string NameAndSurname { get; set; }
+        public string RegisterOfPhysicalPerson { get; set; }
+        public DateTime DateOfBorn { get; set; }
+    }
+}
