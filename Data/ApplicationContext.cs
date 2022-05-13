208a8c481618c597cffb4b8733ef6b5c571a8004
Gabriel Nascimento <gabrielnascimento_17@hotmail.com>
2022-05-13 00:24:27 -0300

    fix: method for add column name 'HasField()' is incorrrect. the correct is 'HasColumnName'

--- Data/ApplicationContext.cs
+++ Data/ApplicationContext.cs
@@ -16,9 +16,9 @@ namespace MonitoriaWEBAPI.Data
             modelBuilder.Entity<Client>(table => {
 
                 table.ToTable("clients");
-                table.Property("NameAndSurname").HasMaxLength(40).HasField("name_and_surname");
-                table.Property("RegisterOfPhysicalPerson").HasColumnType("CHAR(11)").HasField("register_of_physical_person");
-                table.Property("DateOfBorn").HasColumnType("DATE").HasField("date_of_born");
+                table.Property("NameAndSurname").HasMaxLength(40).HasColumnName("name_and_surname");
+                table.Property("RegisterOfPhysicalPerson").HasColumnType("CHAR(11)").HasColumnName("register_of_physical_person");
+                table.Property("DateOfBorn").HasColumnType("DATE").HasColumnName("date_of_born");
 
                 table.HasKey("ClientId").HasName("client_id");
             });   
